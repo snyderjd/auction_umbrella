772339a557c9169acb2fde64a76ad8384356bcb8
Joe Snyder <snyderjosephd@yahoo.com>
2021-02-03 08:14:09 -0600

    Added more tests and documentation

--- apps/auction/test/auction_test.exs
+++ apps/auction/test/auction_test.exs
@@ -1,6 +1,7 @@
 defmodule AuctionTest do
   use ExUnit.Case
   alias Auction.{Item, Repo}
+  import Ecto.Query
   doctest Auction
 
   # setup takes a block as its argument and runs that block before each and every associated test
@@ -23,6 +24,44 @@ defmodule AuctionTest do
     # Pattern-matches the passed map and assigns it to an items variable for use in the test
     test "returns all Items in the database", %{items: items} do
       assert items == Auction.list_items()
+    end
+  end
+
+  # Insert two items and make sure only get one back (the correct one)
+  describe "get_item/1" do
+    setup do
+      {:ok, item1} = Repo.insert(%Item{title: "Item 1"})
+      {:ok, item2} = Repo.insert(%Item{title: "Item 2"})
+      %{items: [item1, item2]}
+    end
+
+    test "returns a single Item based on id", %{items: items} do
+      item = Enum.at(items, 1)
+      assert item == Auction.get_item(item.id)
+    end
+  end
+
+  describe "insert_item/1" do
+    test "adds an Item to the database" do
+      # Assigns a query to a variable
+      count_query = from i in Item, select: count(i.id)
+
+      before_count = Repo.one(count_query)
+      {:ok, _item} = Auction.insert_item(%{title: "test item"})
+
+      # Re-run the query to make sure there's an additional item now
+      assert Repo.one(count_query) == before_count + 1
+    end
+
+    test "the Item in the database has the attributes provided" do
+      attrs = %{title: "test item", description: "test description"}
+      {:ok, item} = Auction.insert_item(attrs)
+      assert item.title == attrs.title
+      assert item.description == attrs.description
+    end
+
+    test "it returns an error on error" do
+      assert {:error, _changeset} = Auction.insert_item(%{foo: :bar})
     end
   end
 
--- mix.exs
+++ mix.exs
@@ -6,7 +6,8 @@ defmodule AuctionUmbrella.MixProject do
       apps_path: "apps",
       version: "0.1.0",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      name: "auction"
     ]
   end
 
@@ -16,6 +17,9 @@ defmodule AuctionUmbrella.MixProject do
   #
   # Run "mix help deps" for examples and options.
   defp deps do
-    []
+    [
+      # 'dev: true' specifies that the dependency won't be included in production builds
+      {:ex_doc, "~> 0.19", dev: true, runtime: false}
+    ]
   end
 end
